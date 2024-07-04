92fc8e96d90df1d108915a0bad4aaf577684f20e
abinercL <abiner17costa@gmail.com>
2024-07-04 16:41:51 -0300

    Adicionado O useState para gerenciar altura, peso e mensagens

--- oneBithealth/src/components/Form/index.js
+++ oneBithealth/src/components/Form/index.js
@@ -1,7 +1,15 @@
-import React from "react";
+import { useState } from "react";
 import { View, Text, TextInput, Button } from "react-native";
+import ResultImc from "./ResultImc";
 
 export default function Form() {
+
+    const [height, setHeight] = useState(null)
+    const [weight, setWeight] = useState(null)
+    const [messageImc, setMessageImc] = useState("preencha o peso e altura")
+    const [imc, setImc] = useState(null)
+    const [TextButton, setTextButton] = useState("Calcular")
+
     return (
         <View>
             <View>
